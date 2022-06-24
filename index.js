9a51ac267696ec49efc41ba9ee8940d5fac32154
Qwerty <adingzfemz@gmail.com>
2022-06-24 08:06:08 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+// const user = {
+//     name: "Benjamin Odunjo",
+//     height: '170cm',
+//     country: "Nigeria"
+// }
+
+let name = 'Benjamin Odunjo' ;
+let height = '170cm' ;
+let country = 'Nigeria';
+
+console.log(name)
+console.log(height)
+console.log(country)
+// alert(name)
+// alert(height)
+// alert(country)
